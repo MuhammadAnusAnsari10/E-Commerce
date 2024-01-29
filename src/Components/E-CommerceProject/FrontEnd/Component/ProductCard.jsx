6cd16dc9b2b44b4d11e9dbe44a2c1bde95d12a7c
muhammadanusansari <muhammadanusansari10@gmail.com>
2024-01-29 14:26:25 -0800

    RelatedProduct

--- src/Components/E-CommerceProject/FrontEnd/Component/ProductCard.jsx
+++ src/Components/E-CommerceProject/FrontEnd/Component/ProductCard.jsx
@@ -42,8 +42,167 @@ export default function ProductCard() {
     }
   }, [open]);
 
+  ////////////////////////////////////////////////////////
+
+  const [readMore, setReadMore] = useState(false);
   return (
     <>
+      {/* /////////////////////// dialog box start////////////////////////////// */}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll={scroll}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+        maxWidth="lg"
+      >
+        {clickedProduct && (
+          <>
+            <DialogContent dividers={scroll === "paper"}>
+              <Grid container spacing={2}>
+                <Grid item xs={7}>
+                  <img
+                    src={clickedProduct.imageurl}
+                    style={{ width: "100%", height: "auto" }}
+                  />
+                </Grid>
+                <Grid item xs={5}>
+                  <Typography
+                    gutterBottom
+                    variant="h4"
+                    component="div"
+                    sx={{
+                      "&:hover": {
+                        color: "seagreen",
+                        cursor: "pointer",
+                        transition: "0.6s",
+                      },
+                    }}
+                  >
+                    {clickedProduct.productTitle}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    sx={{ margin: "10% 0  " }}
+                  >
+                    {/* {clickedProduct.productDescription} */}
+                    {readMore
+                      ? clickedProduct.productDescription
+                      : clickedProduct.productDescription.slice(0, 100)}
+                    {clickedProduct.productDescription.length > 130 && (
+                      <Typography
+                        component="small"
+                        color="primary"
+                        sx={{ color: "", cursor: "pointer" }}
+                        onClick={() => setReadMore(!readMore)}
+                      >
+                        {readMore ? " Read Less" : " Read More"}
+                      </Typography>
+                    )}
+                  </Typography>
+
+                  <Typography
+                    variant="div"
+                    sx={{
+                      color: "#009F7F",
+                      fontSize: "30px",
+                    }}
+                  >
+                    ${clickedProduct.productPrice}
+                  </Typography>
+                  <Button
+                    sx={{
+                      bgcolor: "#009F7F",
+                      color: "white",
+                      padding: "12px",
+                      margin: "10% 0 0 0 ",
+                      "&:hover": {
+                        bgcolor: "#009F7F",
+                        color: "white",
+                      },
+                    }}
+                  >
+                    Add To Shopping Cart
+                  </Button>
+                </Grid>
+                {/* related products cards  start */}
+                {isProducts
+                  .filter(
+                    (product) =>
+                      product.productCategory ===
+                        clickedProduct.productCategory &&
+                      product.productId !== clickedProduct.productId
+                  )
+                  .map((item) => (
+                    <Grid item xs={3}>
+                      <Card
+                        sx={{
+                          position: "relative",
+                          transition: "0.7s",
+                          "&hover:": { transformY: "translate(20px)" },
+                          transition: "0.7s",
+                        }}
+                        // component={Link}
+                        // to={`singleproduct`}
+                        onClick={handleClickOpen("body", item)}
+                      >
+                        <Chip
+                          label="20%"
+                          size="small"
+                          sx={{
+                            position: "absolute",
+                            top: "2%",
+                            right: "2%",
+                            zIndex: 1,
+                            bgcolor: "seagreen",
+                            color: "white",
+                          }}
+                        />
+
+                        <CardMedia
+                          sx={{ height: "200px" }}
+                          image={item.imageurl}
+                          title={item.productTitle}
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h6" component="div">
+                            ${item.productPrice}
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                            {item.productTitle}
+                          </Typography>
+                        </CardContent>
+                        <CardActions>
+                          <Button
+                            fullWidth
+                            sx={{
+                              bgcolor: "#F3F4F6",
+                              color: "black",
+                              "&:hover": {
+                                bgcolor: "seagreen",
+                                color: "white",
+                              },
+                            }}
+                          >
+                            Add
+                            <AddIcon />
+                          </Button>
+                        </CardActions>
+                      </Card>
+                    </Grid>
+                  ))}
+              </Grid>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Close</Button>
+            </DialogActions>
+            {/* related products cards  end */}
+          </>
+        )}
+      </Dialog>
+      {/* /////////////////////// dialog box end////////////////////////////// */}
+
       <Grid container spacing={3}>
         {isProducts
           .filter(
@@ -54,7 +213,12 @@ export default function ProductCard() {
           .map((item) => (
             <Grid item xs={4} key={item.productId}>
               <Card
-                sx={{ position: "relative" }}
+                sx={{
+                  position: "relative",
+                  transition: "0.7s",
+                  "&hover:": { transformY: "translate(20px)" },
+                  transition: "0.7s",
+                }}
                 // component={Link}
                 // to={`singleproduct`}
                 onClick={handleClickOpen("body", item)}
@@ -99,79 +263,6 @@ export default function ProductCard() {
                   </Button>
                 </CardActions>
               </Card>
-              {/* /////////////////////// dialog box start////////////////////////////// */}
-              <Dialog
-                open={open}
-                onClose={handleClose}
-                scroll={scroll}
-                aria-labelledby="scroll-dialog-title"
-                aria-describedby="scroll-dialog-description"
-              >
-                {clickedProduct && (
-                  <>
-                    <DialogContent dividers={scroll === "paper"}>
-                      <Grid container spacing={2}>
-                        <Grid item xs={7}>
-                          <img
-                            src={clickedProduct.imageurl}
-                            style={{ width: "100%", height: "auto" }}
-                          />
-                        </Grid>
-                        <Grid item xs={5}>
-                          <Typography
-                            gutterBottom
-                            variant="h4"
-                            component="div"
-                            sx={{
-                              "&:hover": {
-                                color: "seagreen",
-                                cursor: "pointer",
-                                transition: "0.6s",
-                              },
-                            }}
-                          >
-                            {clickedProduct.productTitle}
-                          </Typography>
-                          <Typography
-                            variant="body2"
-                            color="text.secondary"
-                            sx={{ margin: "10% 0  " }}
-                          >
-                            {clickedProduct.productDescription}
-                          </Typography>
-                          <Typography
-                            variant="div"
-                            sx={{
-                              color: "#009F7F",
-                              fontSize: "30px",
-                            }}
-                          >
-                            ${clickedProduct.productPrice}
-                          </Typography>
-                          <Button
-                            sx={{
-                              bgcolor: "#009F7F",
-                              color: "white",
-                              padding: "12px",
-                              margin: "10% 0 0 0 ",
-                              "&:hover": {
-                                bgcolor: "#009F7F",
-                                color: "white",
-                              },
-                            }}
-                          >
-                            Add To Shopping Cart
-                          </Button>
-                        </Grid>
-                      </Grid>
-                    </DialogContent>
-                    <DialogActions>
-                      <Button onClick={handleClose}>Close</Button>
-                    </DialogActions>
-                  </>
-                )}
-              </Dialog>
-              {/* /////////////////////// dialog box end////////////////////////////// */}
             </Grid>
           ))}
       </Grid>
